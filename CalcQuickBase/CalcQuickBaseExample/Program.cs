5c0528fa5dbe2f22d5a16f800f8dc7f856f67ac7
Ganesan <ganesanv@syncfusion.com>
2018-11-08 16:38:44 +0530

    The sample files are added.

---
+++ CalcQuickBase/CalcQuickBaseExample/Program.cs
@@ -0,0 +1,36 @@
+using Syncfusion.Calculate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcQuickBaseExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PerformParseAndCompute();
+        }
+
+        private static void PerformParseAndCompute()
+        {
+            CalcQuickBase calcQuick = new CalcQuickBase();
+
+            calcQuick["A"] = "5";
+            calcQuick["B"] = "6";
+            calcQuick["C"] = "11";
+
+            //Computing expressions,
+            calcQuick["result"] = "=([A]+[B])/[C]";
+
+            //Computing In-Built formulas,
+            calcQuick["result1"] = "=SUM([A],[B])";
+
+            Console.WriteLine("The calculated result of formula (([A]+[B])/[C]) : " + calcQuick["result"]);
+            Console.WriteLine("The calculated result of formula (SUM([A],[B])) : " + calcQuick["result1"]);
+            Console.ReadKey();
+        }
+    }
+}
